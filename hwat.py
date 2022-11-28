76377b63f320b1e7d099b3776bbd7200d58d5616
Adam Maximilian Wilson <amawi@oceanus.imm.dtu.dk>
2022-11-28 15:10:52 +0100

    train loop territory

--- hwat.py
+++ hwat.py
@@ -133,28 +133,28 @@ class SampleState():
     struct.PyTreeNode means jax transformations *do not affect it* eg pmap
     fn.apply << apply vs fn() << apply_fn"""
     
-    def __init__(_i, step=0, move_std=0.02, corr_len=20):
+    def __init__(_i, step=0, deltar=0.02, corr_len=20):
         _i.acc_target = 0.5
         _i.corr_len = corr_len//2
-        _i.move_std = move_std
+        _i.deltar = deltar
         _i.step = step
     
     def __call__(_i, x, state:TrainState):
         _i.rng, rng_0, rng_1, rng_move = rnd.split(_i.rng, 4)
 
-        x, acc_0 = sample(rng_0, x, state, _i.corr_len, _i.move_std)
-        move_std_1 = jnp.clip(_i.move_std + 0.001*rnd.normal(rng_move))
-        x, acc_1 = sample(rng_1, x, state, _i.corr_len, move_std_1)
+        x, acc_0 = sample(rng_0, x, state, _i.corr_len, _i.deltar)
+        deltar_1 = jnp.clip(_i.deltar + 0.001*rnd.normal(rng_move))
+        x, acc_1 = sample(rng_1, x, state, _i.corr_len, deltar_1)
 
         mask = jnp.array((_i.acc_target-acc_0)**2 < (_i.acc_target-acc_1)**2, dtype=jnp.float32)
         not_mask = ((mask-1.)*-1.)
-        _i.move_std = mask*_i.move_std + not_mask*move_std_1
+        _i.deltar = mask*_i.deltar + not_mask*deltar_1
         return x
 
-def sample(rng, x, state:TrainState, corr_len, move_std):
+def sample(rng, x, state:TrainState, corr_len, deltar):
 
-    def move(x, rng, move_std):
-        x = x + rnd.normal(rng, x.shape)*move_std
+    def move(x, rng, deltar):
+        x = x + rnd.normal(rng, x.shape)*deltar
         return x
 
     to_prob = lambda log_psi: jnp.exp(log_psi)**2
@@ -165,7 +165,7 @@ def sample(rng, x, state:TrainState, corr_len, move_std):
     for _ in range(corr_len//2):
         rng, rng_move, rng_alpha = rnd.split(rng)
         
-        x_1 = move(x, rng_move, move_std)
+        x_1 = move(x, rng_move, deltar)
         p_1 = to_prob(state(x_1))
 
         p_mask = (p_1 / p) > rnd.uniform(rng_alpha, p_1.shape)
