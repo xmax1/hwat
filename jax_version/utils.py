82ff8775413ab9f2838fa209c9ba5bafc3e9b78c
Adam Maximilian Wilson <amawi@oceanus.imm.dtu.dk>
2022-12-13 17:00:52 +0100

    run_things

--- jax_version/utils.py
+++ jax_version/utils.py
@@ -166,6 +166,7 @@ def run_cmds_server(server:str, user:str, cmd:str|list, cwd=str|Path):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # if not known host
     client.connect(server, username=user)
     for cmd_1 in (cmd if isinstance(cmd, list) else [cmd]):
+        print(cwd, cmd_1)
         stdin, stdout, stderr = client.exec_command(f'cd {str(cwd)}; {cmd_1}')
         print('stdin:  ', stdin.readlines())
         print('stdout: ', stdout.readlines())
