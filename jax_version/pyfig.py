9b062750f7b6abf28aab2abe038394bef9f0c9a5
max <amawi@dtu.dk>
2022-12-13 11:18:39 +0100

    jax separate testing stage

---
+++ jax_version/pyfig.py
@@ -0,0 +1,250 @@
+from utils import flat_any
+import inspect
+from typing import Callable
+from functools import reduce, partial
+from simple_slurm import Slurm
+import wandb
+from pathlib import Path
+import sys
+from pprint import pprint
+from copy import copy
+import numpy as np
+
+from utils import run_cmds, run_cmds_server, count_gpu, gen_alphanum, iterate_folder
+from utils import flat_dict, mkdir, cmd_to_dict, dict_to_wandb
+from utils import Sub
+
+from _user import user, server
+
+docs = 'https://www.notion.so/5a0e5a93e68e4df0a190ef4f6408c320'
+
+class Pyfig:
+
+    project_root:   str     = Path().home()/'projects'
+    project:        str     = 'hwat'
+
+    data_dir:       Path    = project_root / 'data'  # not a property, might change
+    run_path:       Path    = property(lambda _: _.project_path / 'run.py')
+    exp_name:       str     = 'demo-final'
+    exp_id:         str     = gen_alphanum(n=7)
+    
+    
+    seed:           int     = 808017424 # grr
+    dtype:          str     = 'float32'
+    n_step:         int     = 200
+    log_metric_step:int     = 100
+    log_state_step: int     = 10          
+	
+    class data(Sub):
+        """
+        n_e = \sum_i charge_nuclei_i - charge = n_e
+        spin = n_u - n_d
+        n_e = n_u + n_d
+        n_u = 1/2 ( spin + n_e ) = 1/2 ( spin +  \sum_i charge_nuclei_i - charge)
+        """
+        charge:     int         = 0
+        spin:       int         = 0
+        a:          np.ndarray  = np.array([[0.0, 0.0, 0.0],])
+        a_z:        np.ndarray  = np.array([4.,])
+
+        n_e:        int         = property(lambda _: int(sum(_.a_z)))
+        n_u:        int         = property(lambda _: (spin + _.n_e)//2 )
+        n_d:        int         = property(lambda _: _.n_e - _.n_u)
+        
+        n_b:        int         = 512
+        n_corr:     int         = 20
+        n_equil:    int         = 10000  # total number: n_equil_loop = equil_len / corr_len
+        acc_target: int         = 0.5
+        
+    class model(Sub):
+        with_sign:      bool    = False
+        n_sv:           int     = 32
+        n_pv:           int     = 16
+        n_fbv:          int     = property(lambda _: _.n_sv*3+_.n_pv*2)
+        n_fb:           int     = 2
+        n_det:          int     = 1
+        terms_s_emb:    list    = ['ra', 'ra_len']
+        terms_p_emb:    list    = ['rr', 'rr_len']
+
+    class sweep(Sub):
+        method          = 'random'
+        name            = 'sweep'
+        metrics         = dict(
+            goal        = 'minimize',
+            name        = 'validation_loss',
+        )
+        parameters = dict(
+            batch_size  = {'values' : [16, 32, 64]},
+            # epoch       = {'values' : [5, 10, 15]},
+            lr          = {'max'    : 0.1, 'min': 0.0001},
+        )
+        n_sweep         = reduce(
+            lambda i,j:i*j,[len(v['values']) for k,v in parameters.items() if 'values' in v])+1
+        sweep_id = ''
+
+    class wandb_c(Sub):
+        job_type        = 'training'
+        entity          = 'hwat'
+        wandb_run_path  = ''
+
+    class slurm(Sub):
+        mail_type       = 'FAIL'
+        partition       ='sm3090'
+        nodes           = 1                # n_node
+        ntasks          = 8                # n_cpu
+        cpus_per_task   = 1     
+        time            = '0-12:00:00'     # D-HH:MM:SS
+        gres            = 'gpu:RTX3090:1'
+        output          = property(lambda _: _._p.TMP /'o-%j.out')
+        error           = property(lambda _: _._p.TMP /'e-%j.err')
+        job_name        = property(lambda _: _._p.exp_name)  # this does not call the instance it is in
+        sbatch          = property(lambda _: )
+        sbatch_cmd = f""" 
+        module purge 
+        source ~/.bashrc 
+        module load GCC 
+        module load CUDA/11.4.1 
+        module load cuDNN/8.2.2.26-CUDA-11.4.1 
+        conda activate {_._p.env} 
+        export MKL_NUM_THREADS=1 
+        export NUMEXPR_NUM_THREADS=1 
+        export OMP_NUM_THREADS=1 
+        export OPENBLAS_NUM_THREADS=1
+        pwd
+        nvidia-smi
+        mv_cmd = f'mv {_._p.TMP}/o-$SLURM_JOB_ID.out {_._p.TMP}/e-$SLURM_JOB_ID.err $out_dir' 
+        """
+
+    project_path:       Path    = property(lambda _: _.project_root / _.project)
+    server_project_path:Path    = property(lambda _: _.project_path)
+    n_device:           int     = property(lambda _: count_gpu())
+
+    exp_path:           Path    = property(lambda _: iterate_folder(_.project_exp_dir/_.exp_name,_.iter_exp_dir)/_.exp_id)
+    TMP:                Path    = Path('./dump/tmp')
+
+    server:             str     = server   # SERVER
+    user:               str     = user             # SERVER
+    git_remote:         str     = 'origin'      
+    git_branch:         str     = 'main'        
+    env:                str     = 'dex'                 # CONDA ENV
+    
+    submit_state:       int  = -1
+    _sys_arg:           list = sys.argv[1:]
+    _wandb_ignore:      list = ['sbatch',]
+
+    def __init__(ii,args:dict={},cap=40,sweep=False,wandb_mode='online',get_sys_arg=True):
+        
+        for k,v in Pyfig.__dict__.items():
+            if isinstance(v, type):
+                v = v(parent=ii)
+                setattr(ii, k, v)
+        
+        ii.merge(cmd_to_dict(sys.argv[1:], ii.d))
+        ii.merge(args)
+
+        mkdir(ii.exp_path)
+        
+        run = wandb.init(
+            job_type    = ii.wandb_c.job_type,
+            entity      = ii.wandb_c.entity,
+            project     = ii.project,
+            dir         = './dump', # ii.exp_path,
+            config      = dict_to_wandb(ii.d, ignore=ii._wandb_ignore),
+            mode        = wandb_mode,
+            settings = wandb.Settings(start_method='fork'), # idk y this is issue, don't change
+        )
+
+        if ii.submit_state > 0:
+            n_job_running = run_cmds([f'squeue -u {ii.user} -h -t pending,running -r | wc -l'])
+            if n_job_running > cap:
+                exit(f'There are {n_job_running} on the submit cap is {cap}')
+            print(f'{n_job_running} on the cluster')
+
+            _slurm = Slurm(**ii.slurm.d)
+            n_run, ii.submit_state = ii.submit_state, 0            
+            for _ in range(n_run):
+                _slurm.sbatch(ii.slurm.sbatch \
+                    + f'out_dir={(mkdir(ii.exp_path/"out"))} {ii.cmd} | tee $out_dir/py.out date "+%B %V %T.%3N" ')
+
+        ii.wandb_c.wandb_run_path = run.path  
+        print('run: ', run.path, '✅')
+
+    @property
+    def cmd(ii,):
+        d = flat_dict(ii.d)
+        return ' '.join([f' --{k}  {str(v)} ' for k,v in d.items()])
+
+    @property
+    def d(ii, ignore=['d', 'cmd', 'submit', 'partial', 'sweep']):
+        out = {}
+        for k,v in ii.__class__.__dict__.items():
+            if k.startswith('_') or k in ignore:
+                continue
+            if isinstance(v, partial):
+                v = copy(ii.__dict__[k]) # ‼ 🏳 Danger zone - partials may not be part of dict
+            else:
+                v = getattr(ii, k)
+            if isinstance(v, Sub): 
+                v = v.d
+            out[k] = v
+        return out
+
+    @property
+    def _sub_cls(ii):
+        return [v for v in ii.__dict__.values() if isinstance(v, Sub)]
+    
+    def submit(ii, sweep=False, commit_msg=None, commit_id=None):
+        commit_msg = commit_msg or ii.exp_id
+        
+        ii.submit_state *= -1
+        if ii.submit_state > 0:
+            if sweep:
+                ii.sweep_id = wandb.sweep(
+                    env     = f'conda activate {ii.env};',
+                    sweep   = ii.sweep.d, 
+                    program = ii.run_path,
+                    project = ii.project,
+                    name    = ii.exp_name,
+                    run_cap = ii.sweep.n_sweep
+                )
+                ii.submit_state *= ii.sweep.n_sweep
+            
+            local_out = run_cmds(['git add .', f'git commit -m {commit_msg}', 'git push'], cwd=ii.project_path)
+            cmd = f'python -u {ii.run_path} ' + (commit_id or ii.commit_id) + ii.cmd
+            server_out = run_cmds_server(ii.server, ii.user, cmd, cwd=ii.server_project_path)
+
+    def partial(ii, f:Callable, get_dict=False, **kw):
+        d = flat_any(ii.d)
+        d_k = inspect.signature(f.__init__).parameters.keys()
+        d = {k:copy(v) for k,v in d.items() if k in d_k}
+        d = {k:v for k,v in d.items() if k in d_k} | kw
+        if get_dict:
+            return d
+        return f(**d)
+
+    def merge(ii, d:dict):
+        for k,v in d.items():
+            for cls in [ii]+ii._sub_cls:
+                if k in cls.__class__.__dict__:
+                    try:
+                        setattr(cls, k, copy(v))
+                    except Exception:
+                        print(f'Unmerged {k}')
+            
+    def _debug_print(ii, on=False, cls=True):
+        if on:
+            for k in vars(ii).keys():
+                if not k.startswith('_'):
+                    print(k, getattr(ii, k))    
+            if cls:
+                [print(k,v) for k,v in vars(ii.__class__).items() if not k.startswith('_')]
+
+    @property
+    def commit_id(ii,)->str:
+        process = run_cmds(['git log --pretty=format:%h -n 1'], cwd=ii.project_path)[0]
+        return process.stdout.decode('utf-8')  
+
+
+
+        
+
---
+++ jax_version/utils.py
@@ -0,0 +1,288 @@
+from pathlib import Path
+from jax import numpy as jnp
+import paramiko
+import subprocess
+from time import sleep
+from itertools import product
+import random
+from typing import Any, Iterable
+import re
+from ast import literal_eval
+import numpy as np
+import os
+import jax
+
+import numpy as np
+from copy import copy
+
+this_dir = Path(__file__).parent
+
+### Pyfig Subclass 
+
+class Sub:
+    _p = None
+
+    def __init__(ii, parent=None):
+        ii._p = parent
+    
+    @property
+    def d(ii, ignore=['d', 'cmd', '_p']):
+        out={} # becomes class variable in call line, accumulates
+        for k,v in ii.__class__.__dict__.items():
+            if k.startswith('_') or k in ignore:
+                continue
+            if isinstance(v, partial): 
+                v = ii.__dict__[k]   # if getattr then calls the partial, which we don't want
+            else:
+                v = getattr(ii, k)
+            out[k] = v
+        return out
+
+### metrics ###
+
+def collect_stats(k, v, new_d, p='tr', suf='', sep='/', sep_long='-'):
+	depth = p.count('/')
+	if depth > 1:
+		sep = sep_long
+	if isinstance(v, dict):
+		for k_sub,v_sub in v.items():
+			collect_stats(k, v_sub, new_d, p=(p+sep+k_sub))
+	else:
+		new_d[p+sep+k+suf] = v
+	return new_d
+
+### debug things
+
+def debug(on=False):
+    if on:
+        os.environ['debug'] = 'debug'
+    else:
+        os.environ['debug'] = ''
+
+def wpr(d:dict):
+    if os.environ.get('debug') == 'debug':
+        for k,v in d.items():
+            typ = type(v) 
+            has_shape = hasattr(v, 'shape')
+            shape = v.shape if has_shape else None
+            dtype = v.dtype if hasattr(v, 'dtype') else None
+            mean = jnp.mean(v) if has_shape else v
+            std = jnp.std(v) if has_shape else None
+            print(k, f'\t mean={mean} \t std={std} \t shape={shape} \t dtype={dtype}') # \t type={typ}
+
+### count things
+
+def count_gpu() -> int: 
+    # output = run_cmd('echo $CUDA_VISIBLE_DEVICES', cwd='.') - one day 
+    import os
+    return sum(c.isdigit() for c in os.environ.get('CUDA_VISIBLE_DEVICES'))
+
+def get_cartesian_product(*args):
+    """ Cartesian product is the ordered set of all combinations of n sets """
+    return list(product(*args))
+
+def zip_in_n_chunks(arg: Iterable[Any], n: int) -> zip:   
+    return zip(*([iter(arg)]*n))
+
+def gen_alphanum(n: int = 7, test=False):
+    from string import ascii_lowercase, ascii_uppercase
+    random.seed(test if test else None)
+    numbers = ''.join([str(i) for i in range(10)])
+    characters = ascii_uppercase + ascii_lowercase + numbers
+    name = ''.join([random.choice(characters) for _ in range(n)])
+    return name
+
+def iterate_folder(folder: Path, iter_exp_dir):
+    if iter_exp_dir and folder.exists():
+        for i in range(100):
+            _folder = add_to_Path(folder, f'-{i}')
+            if not re.search(_folder.name, f'-[0-9]*'):
+                folder = _folder
+                break
+        else:
+            folder = add_to_Path(folder, f'-0')
+    return folder
+
+### do things
+
+def mkdir(path: Path) -> Path:
+    path = Path(path)
+    if path.suffix != '':
+        path = path.parent
+    if path.exists():
+        print('path exists, leaving alone')
+    else:
+        path.mkdir(parents=True)
+    return path
+
+def add_to_Path(path: Path, string: str | Path):
+        return Path(str(path) + str(string))
+
+### convert things
+
+def npify(v):
+    return jnp.array(v.numpy())
+
+
+def cmd_to_dict(cmd:str|list,ref:dict,_d={},delim:str=' --'):
+    """
+    fmt: [--flag, arg, --true_flag, --flag, arg1]
+    # all flags double dash because of negative numbers duh """
+    booleans = ['True', 'true', 't', 'False', 'false', 'f']
+    
+    cmd = ' '.join(cmd) if isinstance(cmd, list) else cmd
+    cmd = [x.lstrip().lstrip('--').rstrip() for x in cmd.split(delim)]
+    cmd = [x.split(' ', maxsplit=1) for x in cmd if ' ' in x]
+    [x.append('True') for x in cmd if len(x) == 1]
+    cmd = flat_list(cmd)
+    cmd = iter([x.strip() for x in cmd])
+
+    for k,v in zip(cmd, cmd):
+        if v in booleans: 
+            v=booleans.index(v)<3  # 0-2 True 3-5 False
+        if k in ref:
+            _d[k] = type(ref[k])(v)
+        else:
+            try:
+                _d[k] = literal_eval(v)
+            except:
+                _d[k] = str(v)
+            print(f'Guessing type: {k} as {type(v)}')
+    return _d
+
+### run things
+
+def run_cmds(cmd:str|list,cwd:str|Path=None,input_req:str=None):
+    _out = []
+    for cmd_1 in (cmd if isinstance(cmd, list) else [cmd]): 
+        cmd_1 = [c.strip() for c in cmd_1.split(' ')]
+        _out += [subprocess.run(
+            cmd_1,cwd=cwd,input=input_req, capture_output=True)]
+        sleep(0.1)
+    return _out
+
+def run_cmds_server(server:str,user:str,cmd:str|list,cwd=str|Path):
+    _out = []
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # if not known host
+    client.connect(hostname=server, username=user)
+    client.exec_command(f'cd {cwd}')
+    with client as _r:
+        for cmd_1 in (cmd if isinstance(cmd, list) else [cmd]):
+            _out += [_r.exec_command(f'{cmd_1}')] # in, out, err
+            sleep(0.1)
+    return _out
+
+
+# flatten things
+
+def flat_arr(v):
+    return v.reshape(v.shape[0], -1)
+
+def flat_list(lst_of_lst):
+    return [lst for sublst in lst_of_lst for lst in sublst]
+
+def flat_dict(d:dict,_l:list=[]):
+    for k,v in d.items():
+        if isinstance(v, dict):
+            _l.extend(flat_dict(v, _l=_l).items())
+        else:
+            _l.append((k, v))
+    return dict(_l)
+
+def flat_any(v: list|dict|jnp.ndarray|np.ndarray):
+    if isinstance(v, list):
+        return flat_list(v)
+    if isinstance(v, dict):
+        return flat_dict(v)
+
+
+### wandb ###
+
+def dict_to_wandb(
+    d:dict, 
+    parent='', 
+    sep='.', 
+    ignore=[],
+    _l:list=None,
+    )->dict:
+    _l = [] if _l is None else _l
+    for k, v in d.items():
+        if isinstance(v, Path) or callable(v):
+            continue
+        if k in ignore:
+            continue
+        k_1 = parent + sep + k if parent else k
+        if isinstance(v, dict):
+            _l.extend(dict_to_wandb(v, k_1, _l=_l).items())
+        elif callable(v):
+            continue
+        _l.append((k_1, v))
+    return dict(_l)
+
+
+### jax ###
+
+try:
+    from flax.core.frozen_dict import FrozenDict
+    from jax import random as rnd
+    
+    def gen_rng(rng, n_device):
+        """ rng generator for multi-gpu experiments """
+        rng, rng_p = jnp.split(rnd.split(rng, n_device+1), [1,])
+        return rng.squeeze(), rng_p	
+        
+
+    def compute_metrix(d:dict, mode='tr', fancy=None, ignore = [], _d = {}):
+        
+        for k,v in d.items():
+            if any([ig in k for ig in ignore+['step']]):
+                continue 
+            
+            if not fancy is None:
+                k = fancy.get(k, k)
+
+            v = jax.device_get(v)
+            
+            if isinstance(v, FrozenDict):
+                v = v.unfreeze()
+            
+            v_mean = jax.tree_map(lambda x: x.mean(), v) if not np.isscalar(v) else v
+            v_std = jax.tree_map(lambda x: x.std(), v) if not np.isscalar(v) else 0.
+
+            group = mode
+            if 'grad' in k:
+                group = mode + '/grad'
+            elif 'param' in k:
+                group += '/param'
+                
+            _d = collect_stats(k, v_mean, _d, p=group, suf=r'_\mu$')
+            _d = collect_stats(k, v_std, _d, p=group+'/std', suf=r'_\sigma$')
+
+        return _d
+
+    ### type testing ### 
+
+    def test_print_fp16_no_cast():
+        x = jnp.ones([1], dtype='float16')
+        print(x)  # FAILS
+
+    def test_print_fp16():
+        x = jnp.ones([1], dtype='float16')
+        x = x.astype('float16')
+        print(x)  # OK
+
+    def test_print_fp32():
+        x = jnp.ones([1], dtype='float16')
+        x = x.astype('float16')
+        x = x.astype('float32')
+        print(x)  # OK
+
+    def test_print_fp32_to_fp16_cast():
+        x = jnp.ones([1], dtype='float32')
+        x = x.astype('float16')
+        print(x)  # FAILS
+
+except:
+    print('no flax or jax installed')
+
