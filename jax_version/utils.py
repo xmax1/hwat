f355570d5d227669b2adc04a7e7a0b72d0de81a9
Adam Maximilian Wilson <amawi@oceanus.imm.dtu.dk>
2022-12-15 23:30:35 +0100

    "run_things"

--- jax_version/utils.py
+++ jax_version/utils.py
@@ -226,9 +226,8 @@ def run_cmds_server(server:str, user:str, cmd:str|list, cwd=str|Path):
     client.connect(server, username=user)
     for cmd_1 in (cmd if isinstance(cmd, list) else [cmd]):
         print(cwd, '\n', cmd_1)
-        res = client.exec_command(f'cd {str(cwd)}; {cmd_1}')[0]
-        print(res)
-        out += [res.stdout, res.stderr]
+        stdin, stdout, stderr = client.exec_command(f'cd {str(cwd)}; {cmd_1}')
+        out += [stdout.readlines(), stderr.readlines()]
         [print(l) for l in out[-1]]
     client.close()
     for res in out:
